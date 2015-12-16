551fc1512da2c7f7f23adb4d0973a6986183de82
liruihao <liruihao@baidu.com>
2015-12-16 11:34:33 +0800

    add data.h

---
+++ data.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file data.h
+ * @author liruihao(com@baidu.com)
+ * @date 2015/12/16 11:31:00
+ * @brief 
+ *  
+ **/
+
+
+
+
+#ifndef  BROCBUILD_DATA_DATA_H
+#define  BROCBUILD_DATA_DATA_H
+
+struct datastruct{
+    int key;
+    int value;
+};
+
+
+
+#endif  //BROCBUILD_DATA_DATA_H
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
